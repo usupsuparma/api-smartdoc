c7e12b1a18d5788f00cd22debd0dd063ea9e0f33
Adam Lesmana Ganda Saputra <aelgees.dev@gmail.com>
2020-03-04 18:45:36 +0700

    CORS

--- app/Modules/Menu/Route/MenuRoutes.php
+++ app/Modules/Menu/Route/MenuRoutes.php
@@ -19,7 +19,7 @@ class MenuRoutes extends BaseRoutes
 		$app->router->group([
 			'prefix' => $this->route_prefix,
 			'namespace' => $this->controller_ns,
-			'middleware' => ['auth', 'cors']
+			'middleware' => 'auth'
 		], function () use ($app) {
 			$app->router->get('/', [
 				'as' => $this->route_prefix . '.data',
