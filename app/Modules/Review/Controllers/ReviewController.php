70a7b0a9f8969c505968aa4d0e0dc6dea0126598
Adam Lesmana Ganda Saputra <aelgees.dev@gmail.com>
2020-03-30 14:01:28 +0700

    Review

--- app/Modules/Review/Controllers/ReviewController.php
+++ app/Modules/Review/Controllers/ReviewController.php
@@ -15,53 +15,53 @@ class ReviewController extends BaseController
 	public function __construct(ReviewRepositories $reviewRepository) 
 	{
 		$this->reviewRepository = $reviewRepository;
-		// Authority::acl_access(Auth::user(), 'reviews');
+		Authority::acl_access(Auth::user(), 'reviews');
 	}
 	
 	public function data(Request $request)
 	{
-		// Authority::check('read');
+		Authority::check('read');
 		
 		return $this->showAll($this->reviewRepository->data($request), 200);
 	}
 	
 	public function show($id)
 	{
-		// Authority::check('read');
+		Authority::check('read');
 		
 		return $this->showOne(ReviewModel::findOrFail($id), 200);
 	}
 	
 	public function data_detail(Request $request, $id)
 	{
-		// Authority::check('read');
+		Authority::check('read');
 		
 		return $this->showAll($this->reviewRepository->data_detail($request, $id), 200);
 	}
 	
 	public function show_detail($review_id, $id)
 	{
-		// Authority::check('read');
+		Authority::check('read');
 		return $this->showOne($this->reviewRepository->show_detail($id), 200);
 	}
 	
 	public function create_detail(Request $request, $id)
 	{
-		// Authority::check('create');
+		Authority::check('create');
 		
         return $this->successResponse($this->reviewRepository->create_detail($request, $id), 200); 
 	}
 	
 	public function update_detail(Request $request, $id)
     {
-		// Authority::check('update');
+		Authority::check('update');
 		
 		return $this->successResponse($this->reviewRepository->update_detail($request, $id), 200); 
 	}
 	
 	public function delete_detail($review_id, $id)
     {
-		// Authority::check('delete');
+		Authority::check('delete');
 		
         return $this->successResponse($this->reviewRepository->delete_detail($id), 200); 
     }
