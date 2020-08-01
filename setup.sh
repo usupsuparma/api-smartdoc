d4179cefcd00e0a278a96eda3d4f808919e81851
Adam Lesmana Ganda Saputra <aelgees.dev@gmail.com>
2020-08-01 14:51:49 +0700

    Setup

---
+++ setup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+echo "=== Smartdoc BIJB Setup Bash Script ===="
+echo "!CANCEL this operation if not sure!"
+
+echo "Please enter deployment target command ?"
+read -p 'setup: ' setup
+
+if [ "$target" == "letsgo" ]; then
+    git remote add upstream git@github.com:aelgees/api-smartdoc-bijb.git
+	echo "!...Fetching latest working copy from UPSTREAM...!"
+	git fetch upstream
+	echo "!...Finish fetching from upstream...!"
+	composer install
+    echo "Composer Install Finished Successfuly"
+	cp .env.example .env
+	echo "Copy File .env.example -> .env Successfuly"
+	php artisan migrate
+	echo "Database Migration Finish Successfuly"
+	php artisan db:seed
+	echo "Database Seeder Finish Successfuly"
+	cp ./public/assets/fonts/edwardian-script-itc.ttf ./vendor/tecnickcom/tcpdf/tools/edwardian-script-itc.ttf
+	./vendor/tecnickcom/tcpdf/tools/tcpdf_addfont.php -b -t TrueTypeUnicode -i ./vendor/tecnickcom/tcpdf/tools/edwardian-script-itc.ttf
+	echo "Finish Update Font PDF Successfuly"
+	php artisan passport:install --force
+	echo "Finish Generate Passport For Website"
+	php artisan passport:install --force
+	echo "Finish Generate Passport For Mobile"
+	mkdir -p storage/app/public/outgoing_mail
+	mkdir -p storage/app/public/incoming_mail
+	mkdir -p storage/app/public/disposition
+	mkdir -p storage/app/public/qr_code
+	mkdir -p storage/app/public/digital_signature
+	chown -R www-data:www-data storage
+else
+    echo "ERROR INVALID COMMAND !!!"
+fi
