2f1410fc6480ce07135241999f3dd3eaa1394ae1
Adam Lesmana Ganda Saputra <aelgees.dev@gmail.com>
2020-09-29 08:01:30 +0700

    alter table oitgoing mail follow up

---
+++ database/migrations/2020_09_29_075712_alter_outgoing_mails_follow_up_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterOutgoingMailsFollowUpTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('outgoing_mails_follow_up', function (Blueprint $table) {
+            $table->string('path_to_file')->nullable()->change();
+        });
+        
+        Schema::table('outgoing_mails_follow_up', function (Blueprint $table) {
+            $table->tinyInteger('progress')->after('path_to_file')->default(2);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
